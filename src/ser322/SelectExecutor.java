0c488ff59e7889a2485298b387a9efa6aa01152a
Daryl Kyle <gdn015@gmail.com>
2024-02-22 20:24:49 -0800

    add missing cases for Select

--- src/ser322/SelectExecutor.java
+++ src/ser322/SelectExecutor.java
@@ -3,6 +3,7 @@ package ser322;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class SelectExecutor extends Executor{
     Scanner in;
@@ -29,6 +30,14 @@ public class SelectExecutor extends Executor{
                     break;
                 case "3":
                     listGamesByGenre();
+                    break;
+                case "4": listGamesRatedOverX();
+                    break;
+                case "5": listGamesByPubAndPlat();
+                    break;
+                case "6": listGamesByNumOfPlayers();
+                    break;
+                case "7": listVoiceActors();
                     break;
                 case "b":
                     System.out.println("Going back to previous menu");
@@ -69,10 +78,12 @@ public class SelectExecutor extends Executor{
                     + "FROM VIDEO_GAME "
                     + "ORDER BY PRICE DESC");
 
+            System.out.println("Game Title \t\tPrice");
+            System.out.println("========== \t\t=====");
             while (rs.next()) {
-                System.out.println("Game: " + rs.getString("GameTitle")
-                        + ", Price: " + rs.getFloat("Price"));
+                System.out.println(rs.getString("GameTitle") + "\t\t $" + rs.getString("Price"));
             }
+            System.out.println();
 
         } catch (SQLException e) {
             System.out.println("Invalid SQL query, please fix and try again");
@@ -87,10 +98,12 @@ public class SelectExecutor extends Executor{
                     + "FROM VIDEO_GAME "
                     + "ORDER BY ESRB_RATING");
 
+            System.out.println("Game Title \tESRB Rating");
+            System.out.println("========== \t===========");
             while (rs.next()) {
-                System.out.println("Game: " + rs.getString("GameTitle")
-                        + ", ESRB_Rating: " + rs.getString("ESRB_Rating"));
+                System.out.println(rs.getString("GameTitle") + "\t" + rs.getString("ESRB_Rating"));
             }
+            System.out.println();
 
         } catch (SQLException e) {
             System.out.println("Invalid SQL query, please fix and try again");
@@ -105,15 +118,131 @@ public class SelectExecutor extends Executor{
                     + "FROM VIDEO_GAME "
                     + "ORDER BY Genre");
 
+            System.out.println("Game Title \tGenre");
+            System.out.println("========== \t=====");
             while (rs.next()) {
-                System.out.println("Game: " + rs.getString("GameTitle")
-                        + ", Genre: " + rs.getString("Genre"));
+                System.out.println(rs.getString("GameTitle") + "\t" + rs.getString("Genre"));
             }
+            System.out.println();
+
+        } catch (SQLException e) {
+            System.out.println("Invalid SQL query, please fix and try again");
+        }
+    }
+    
+    private void listGamesRatedOverX() {
+        int rating = 0;
+        int MIN = 1;
+        int MAX = 5;
+        Scanner scanner = new Scanner(System.in);
+        while(rating < MIN || rating > MAX) {
+            try {
+                System.out.print("Select a rating from " + MIN + " to " + MAX + ": ");
+                rating = scanner.nextInt();
+            }catch(InputMismatchException e) {
+                System.out.println("Invalid selection.");
+                scanner.nextLine();
+            }
+        }
+        
+        try {
+            stmt = connect.createStatement();
+
+            rs = stmt.executeQuery("SELECT VIDEO_GAME.GameTitle, REVIEW.Rating "
+                    + "FROM VIDEO_GAME JOIN REVIEW ON VIDEO_GAME.GameTitle = REVIEW.GameTitle "
+                    + "WHERE REVIEW.Rating > " + rating + 
+                    " ORDER BY REVIEW.Rating DESC");
+
+            System.out.println("Game Name \tReview Rating");
+            System.out.println("========= \t=============");
+            while (rs.next()) {
+                System.out.println(rs.getString("GameTitle") + "\t" + rs.getString("Rating"));
+            }
+            System.out.println();
+
+        } catch (SQLException e) {
+            System.out.println("Invalid SQL query, please fix and try again");
+        }
+        
+    }
+    
+
+    private void listGamesByNumOfPlayers() {
+        int number = 0;
+        Scanner scanner = new Scanner(System.in);
+        while(number < 1) {
+            try {
+                System.out.print("Select a number of players: ");
+                number = scanner.nextInt();
+            }catch(InputMismatchException e) {
+                System.out.println("Invalid selection.");
+                scanner.nextLine();
+            }
+        }
+        
+        try {
+            stmt = connect.createStatement();
+
+            rs = stmt.executeQuery("SELECT PlatformName, MaxPlayers "
+                    + "FROM PLATFORM "
+                    + "WHERE MaxPlayers >= " + number +
+                    " ORDER BY MaxPlayers DESC");
+
+            System.out.println("Platform Name \tMax Players");
+            System.out.println("============= \t===========");
+            while (rs.next()) {
+                System.out.println(rs.getString("PlatformName") + "\t" + rs.getString("MaxPlayers"));
+            }
+            System.out.println();
 
         } catch (SQLException e) {
             System.out.println("Invalid SQL query, please fix and try again");
         }
     }
 
-}
+    private void listGamesByPubAndPlat() {
+        String publisher, platform = null;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of the publisher: ");
+        publisher = scanner.nextLine();
+        System.out.print("Enter the name of the platform: ");
+        platform = scanner.nextLine();
+        
+        try {
+            stmt = connect.createStatement();
 
+            rs = stmt.executeQuery("SELECT VIDEO_GAME.GameTitle\r\n"
+                    + "FROM VIDEO_GAME JOIN PLAYED_ON ON VIDEO_GAME.GameTitle = PLAYED_ON.GameTitle\r\n"
+                    + "WHERE VIDEO_GAME.PublisherName = '" + publisher + "' AND PLAYED_ON.PlatformName = '" + platform + "'");
+
+            System.out.println("Game Title");
+            System.out.println("==========");
+            while (rs.next()) {
+                System.out.println(rs.getString("GameTitle"));
+            }
+            System.out.println();
+
+        } catch (SQLException e) {
+            System.out.println("Invalid SQL query, please fix and try again");
+        }
+        
+    }
+    
+    private void listVoiceActors() {
+        try {
+            stmt = connect.createStatement();
+
+            rs = stmt.executeQuery("SELECT WORKS_FOR.ActorName, WORKS_FOR.PublisherName, APPEARS_IN.GameTitle FROM WORKS_FOR JOIN PUBLISHER ON WORKS_FOR.PublisherName = PUBLISHER.PublisherName JOIN APPEARS_IN ON WORKS_FOR.ActorName = APPEARS_IN.ActorName ORDER BY ActorName ASC");
+
+            System.out.println("Actor Name \tPublisher");
+            System.out.println("========== \t=========");
+            while (rs.next()) {
+                System.out.println(rs.getString("ActorName") + "\t" + rs.getString("PublisherName"));
+            }
+            System.out.println();
+
+        } catch (SQLException e) {
+            System.out.println("Invalid SQL query, please fix and try again");
+        }
+    }
+}
